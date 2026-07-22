1aae231e0beeada14c39fe8684aa462917704b15
Umaimazainab02 <umaimazainab429@gmail.com>
2026-07-22 10:50:28 +0500

    Added loop practice questions

--- app.js
+++ app.js
@@ -82,4 +82,35 @@ else {
 
 
 
-let clock =document.getElementById("clockbtn")
+let clock = document.getElementById("clock");
+let clockbtn = document.getElementById("clockbtn");
+
+clockbtn.addEventListener("click", function () {
+
+    function showTime() {
+        let date = new Date();
+
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let seconds = date.getSeconds();
+
+        if (hours < 10) {
+            hours = "0" + hours;
+        }
+
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+
+        clock.innerText = `${hours}:${minutes}:${seconds}`;
+    }
+
+    showTime(); 
+    setInterval(showTime, 1000);
+
+    clockbtn.disabled = true; 
+});
